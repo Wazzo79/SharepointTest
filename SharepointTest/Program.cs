191ddb2b5ff0aa4b48e983997e822d8aa2d3c8bb
Warren Schwartz <Warren.schwartz@outlook.com>
2016-12-14 14:05:01 +0200

    Add project files.

---
+++ SharepointTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharepointTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ClientContext("**url**"))
+            {
+                context.Credentials = new SharePointOnlineCredentials("**username**", GetPassword("**password**"));
+
+                List list = context.Web.Lists.GetByTitle("SPAR Stores");
+
+                CamlQuery query = CamlQuery.CreateAllItemsQuery(100);
+                ListItemCollection items = list.GetItems(query);
+
+                context.Load(items);
+                context.ExecuteQuery();
+
+                Console.Write(items.Count);
+            }
+        }
+
+        static SecureString GetPassword(string password)
+        {
+            var securePassword = new SecureString();
+
+            foreach (var ch in password)
+                securePassword.AppendChar(ch);
+
+            return securePassword;
+        }
+    }
+}
